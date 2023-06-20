eca4d863e2d9f710e1cbb94efa251dde6002db5f
Ana Maria <ana.msilva@hotmail.com>
2023-06-20 08:14:37 -0300

    feat: add class / style binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CursosModule } from './cursos/cursos.module';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import { ClassBindingComponent } from './class-binding/class-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent,
+    ClassBindingComponent,
     EventBindingComponent,
     PropertyBindingComponent
   ],
